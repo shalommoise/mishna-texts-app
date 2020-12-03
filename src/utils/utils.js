3a352e8e0ea2a209889cbeb6469e8c22308fefb2
Shalommoise <mshalom689@gmail.com>
2020-12-03 12:53:39 +0000

    fixed bug in get numbers x.2-x.5

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,10 +4,15 @@ const getNumbers = (start, end)=>{
 if(!start && !end) return mishnayos;
 const funStart = start*10;
 const funEnd =end*10;
+
+
 mishnayos = [funStart, funStart +1, funEnd -1, funEnd];
-if(Math.round(end)-Math.round(start)){
-   mishnayos = [funStart,Math.floor(start + 1)*10 + 1, funEnd -1, funEnd];
+
+if(Math.round(end)-Math.round(start) > 1){
+
+   mishnayos = [funStart, Math.floor(start + 1)*10 + 1, funEnd -1, funEnd];
 }
+
 if( funEnd.toString().endsWith('3')){
   mishnayos = [funStart, funEnd -2, funEnd -1, funEnd];
 }
